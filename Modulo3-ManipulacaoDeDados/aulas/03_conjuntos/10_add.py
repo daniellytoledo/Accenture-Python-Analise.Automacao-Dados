c0c0b4514666c88cf40c63b8013d05fdfa8c3257
Dani <daniellytoledosouto@gmail.com>
2026-07-03 12:12:34 +0100

    Create 10_add.py

---
+++ Modulo3-ManipulacaoDeDados/aulas/03_conjuntos/10_add.py
@@ -0,0 +1,12 @@
+# adicionando elementos a um conjunto set()
+
+sorteio = {1, 23}
+
+sorteio.add(25)  
+print(sorteio) # {1, 23, 25}
+
+sorteio.add(42)  
+print(sorteio) # {1, 23, 25, 42}
+
+sorteio.add(25)  
+print(sorteio) # {1, 23, 25, 42}
