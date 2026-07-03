3fc0eacce11d2478ec5733f07ff07866c73fe8b8
Dani <daniellytoledosouto@gmail.com>
2026-07-03 11:58:38 +0100

    Update 02_iterar_conjuntos.py

--- Modulo3-ManipulacaoDeDados/aulas/03_conjuntos/02_iterar_conjuntos.py
+++ Modulo3-ManipulacaoDeDados/aulas/03_conjuntos/02_iterar_conjuntos.py
@@ -3,5 +3,7 @@ carros = {"gol", "celta", "palio"}
 for carro in carros:
     print(carro)
 
+# usar enumerate para transformar a lista carros em uma sequência de pares (índice, valor)
+# se formos usar apenas "for indice, carro in carros" daria erro como se indice e carro fossem variáveis com o mesmo resultado 
 for indice, carro in enumerate(carros):
     print(f"{indice}: {carro}")
