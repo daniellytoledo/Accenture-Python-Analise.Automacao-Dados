c3ccd6dea02f6cef2911d67a21553e72bcc7d58c
Dani <daniellytoledosouto@gmail.com>
2026-07-06 20:28:55 +0100

    heranca_multiplica

--- Modulo4-POO/aulas/02_heranca/02_heranca_multipla.py
+++ Modulo4-POO/aulas/02_heranca/02_heranca_multipla.py
@@ -6,19 +6,27 @@ class Animal():
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 class Mamifero(Animal):
-    def __init__(self, nro_patas, cor_pelo):
+    def __init__(self, cor_pelo, **kw):
         self.cor_pelo = cor_pelo
-        super().__init__(nro_patas)
+        super().__init__(**kw)
 
 class Ave(Animal):
-    def __init__(self, nro_patas):
-        super().__init__(nro_patas)
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
 
 class Gato(Mamifero):
     pass
 
-class Onitorrinco(Mamifero):
-    pass
+class Onitorrinco(Mamifero, Ave):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas) 
 
-gato = Gato(4, "preto")
-print(gato)
+onitorrinco = Onitorrinco(
+    nro_patas=2,
+    cor_pelo="vermelho",
+    cor_bico="laranja"
+    )
+
+print(onitorrinco)
+
