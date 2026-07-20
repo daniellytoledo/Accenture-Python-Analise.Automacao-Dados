79db0d7c2288e8ff1b3fd7813dd8e4f4a7cf35dd
Dani <daniellytoledosouto@gmail.com>
2026-07-20 21:05:35 +0100

    Modulo7_introducao

---
+++ Modulo7-Automacao-Rotinas-Visualizacoes/00_automacao_coleta_integracoes.py
@@ -0,0 +1,28 @@
+# Conceito de Automação
+
+# Automação é o processo de executar tarefas automaticamente usando programas. Em vez de executar tarefas manualmente, criamos scripts que realizam essas tarefas de forma automática. Isso permite:
+
+# economizar tempo
+# reduzir erros
+# processar grandes volumes de dados
+
+# Cenários reais de automação
+
+# A automação é usada em várias áreas:
+
+# geração automática de relatórios
+# coleta de dados da internet
+# processamento diário de arquivos
+# atualização de banco de dados
+# monitoramente de sistemas
+
+# Python é muito usado para automação por ser simples e poderoso.
+
+# Manipulação automatizada de arquivos
+
+# Uma das automações mais comuns é processar arquivos automaticamente. Por exemplo:
+
+# ler arquivos de dados
+# calcular métricas
+# gerar relatórios
+
